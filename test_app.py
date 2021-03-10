83dbc0d52a49ccd1ac236c05efcc5ce5e6b27524
Nguyen Hoang Phuong <37414457+hphuongdhsp@users.noreply.github.com>
2021-03-10 18:54:21 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -9,9 +9,18 @@ import cv2
 import errno
 import glob
 import pydicom
+from pydicom.pixel_data_handlers.util import apply_voi_lut
+from tqdm import tqdm
 
+
+
+def boolean_string(s):
+    if s not in {'False', 'True'}:
+        raise ValueError('Not a valid boolean string')
+    return s == 'True'
 
 def has_file_allowed_extension(filename, extensions):
+    
     """Checks if a file is an allowed extension.
     Args:
         filename (string): path to a file
@@ -31,6 +40,7 @@ def mkdir(path):
         if e.errno != errno.EEXIST:
             raise
 
+
 def get_all_items (path): 
     images = []  # list of images
     folder = glob.glob( path +'*') 
@@ -44,46 +54,70 @@ def get_all_items (path):
             folder.remove(f)
     return images
 
+
 def dicom_2_png(PathDicom,output_path):
     '''
     input : dicom file 
     output: write the pixel array of dicom file as type os png as the output path 
     '''
-    ds = pydicom.dcmread(PathDicom)
-    shape = ds.pixel_array.shape
-    # Convert to float to avoid overflow or underflow losses.
-    image_2d = ds.pixel_array.astype(float)
+    try: 
+        ds = pydicom.dcmread(PathDicom)
+        shape = ds.pixel_array.shape
+        # Convert to float to avoid overflow or underflow losses.
+        image_2d = ds.pixel_array.astype(float)
 
-    # Rescaling grey scale between 0-255
-    image_2d_scaled = (np.maximum(image_2d,0) / image_2d.max()) * 255.0
+        # Rescaling grey scale between 0-255
+        image_2d_scaled = (np.maximum(image_2d,0) / image_2d.max()) * 255.0
 
-    # Convert to uint
-    image_2d_scaled = np.uint8(image_2d_scaled)
+        # Convert to uint
+        image_2d_scaled = np.uint8(image_2d_scaled)
 
-    # Write the PNG file
-    with open(output_path, 'wb') as png_file:
-        w = png.Writer(shape[1], shape[0], greyscale=True)
-        w.write(png_file, image_2d_scaled)
+        # Write the PNG file
+        with open(output_path, 'wb') as png_file:
+            w = png.Writer(shape[1], shape[0], greyscale=True)
+            w.write(png_file, image_2d_scaled)
+    except:
+        pass
 
-def dcm2img(input_folder, output_folder):
-    error_list = []
-    item_list = get_all_items(input_folder)
-    for file in item_list: 
-        if has_file_allowed_extension(file, IMG_EXTENSIONS):  ## except only cdm file 
-            dicom_path  = input_folder +  file
-            mkdir( output_folder + '/' + file.rpartition('/')[0])
-            output_path = output_folder + '/' + file.replace('.dcm', '.png')
-            try: 
-                dicom_2_png(dicom_path,output_path)
-            except: 
-                error_list.append(dicom_path)
-    df = pd.DataFrame(error_list,columns =['error_file'])
-    df.to_csv(output_folder + '/' + 'error_list.csv')
-    print('Done')
+def dcm2img(input_folder, output_folder, debug = True):
+    if debug: 
+        error_list = []
+        item_list = get_all_items(input_folder)
+        for file in tqdm(item_list): 
+            if has_file_allowed_extension(file, IMG_EXTENSIONS):  ## except only cdm file 
+                dicom_path  = input_folder +  file
+                mkdir( output_folder + '/' + file.rpartition('/')[0])
+                output_path = output_folder + '/' + file.replace('.dcm', '.png')
+                try: 
+                    dicom_2_png(dicom_path,output_path)
+                except: 
+                    error_list.append(dicom_path)
+        df = pd.DataFrame(error_list,columns =['error_file'])
+        df.to_csv(output_folder + '/' + 'error_list.csv')
+        print('Done')
+    else: 
+        item_list = get_all_items(input_folder)
+        for file in tqdm(item_list): 
+            if has_file_allowed_extension(file, IMG_EXTENSIONS):  ## except only cdm file 
+                dicom_path  = input_folder +  file
+                mkdir( output_folder + '/' + file.rpartition('/')[0])
+                output_path = output_folder + '/' + file.replace('.dcm', '.png')
+                try:
+                    dicom_2_png(dicom_path,output_path)
+                except:
+                    pass
+
+
 if __name__ == "__main__":
-    
-    input_folder = '/home/may-nov/Documents/spine/Torus Imagine'
-    output_folder =  '/home/may-nov/Documents/spine/test1'
+    import argparse
 
-    mkdir(output_folder)
-    dcm2img(input_folder, output_folder)
+    parser = argparse.ArgumentParser(description='dicom2png app python ')
+    parser.add_argument('-i', '--input_dir', type=str, default='/Users/nguyenphuong/Desktop/input-folder', help='the dicom path')
+    parser.add_argument('-o', '--output_dir', type=str, default='/Users/nguyenphuong/Desktop/output-folder', help='the png path')
+    parser.add_argument('-d', '--debug', type = boolean_string, default = True, help='debug or not')
+
+    args = parser.parse_args()
+    
+    mkdir(args.output_dir)
+    dcm2img(args.input_dir, args.output_dir, debug = args.debug)
+
